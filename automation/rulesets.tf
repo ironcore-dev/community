4fa0e11d4de82c3571ffb13cfe5b7c1e64f80e06
Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>
2026-07-09 17:01:26 +0200

    Refactor org rulesets
    Signed-off-by: Daniel Gonzalez Nothnagel <daniel.gonzalez.nothnagel@sap.com>

--- automation/rulesets.tf
+++ automation/rulesets.tf
@@ -1,11 +1,42 @@
-resource "github_organization_ruleset" "default_branch" {
-  name        = "default-branch"
+resource "github_organization_ruleset" "require_signatures" {
+  name        = "Require signatures"
   target      = "branch"
   enforcement = "evaluate"
 
   conditions {
     ref_name {
-      include = ["~DEFAULT_BRANCH"]
+      include = ["~ALL"]
+      exclude = []
+    }
+
+    repository_name {
+      include = ["~ALL"]
+      exclude = []
+    }
+  }
+
+  rules {
+    required_signatures     = true
+
+    commit_message_pattern {
+      name     = "Commit message must contain a Signed-off-by line"
+      operator = "contains"
+      pattern  = "(?m)Signed-off-by: .+ <.+@.+>"
+    }
+  }
+}
+
+resource "github_organization_ruleset" "default_release" {
+  name        = "Protect default and release branches"
+  target      = "branch"
+  enforcement = "evaluate"
+
+  conditions {
+    ref_name {
+      include = [
+        "~DEFAULT_BRANCH",
+        "refs/heads/release-*",
+        ]
       exclude = []
     }
 
@@ -22,43 +53,14 @@ resource "github_organization_ruleset" "default_branch" {
     deletion         = true
     non_fast_forward = true
 
-    required_linear_history = true
-    required_signatures     = true
-
     pull_request {
       required_approving_review_count = 1
-      allowed_merge_methods           = ["squash"]
-
-      # Recommended practices
-      dismiss_stale_reviews_on_push     = true
-      require_code_owner_review         = false # should be set to true once all repos have CODEOWNERS
-      require_last_push_approval        = true
-      required_review_thread_resolution = true
+      allowed_merge_methods           = [
+        "rebase",
+        "squash",
+      ]
     }
-  }
-}
-
-resource "github_organization_ruleset" "dco" {
-  name        = "dco"
-  target      = "branch"
-  enforcement = "evaluate"
-
-  conditions {
-    ref_name {
-      include = ["~DEFAULT_BRANCH"]
-      exclude = []
-    }
-
-    repository_property {
-      include {
-        name            = "visibility"
-        property_values = ["public"]
-        source          = "system"
-      }
-    }
-  }
-
-  rules {
+    
     required_status_checks {
       required_check {
         context        = "DCO"
